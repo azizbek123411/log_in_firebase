c4a210bddf77ceb2ee0f1933c919b3ced14cc0f8
hp <sultonovazizbek73@gmail.com>
2023-10-08 12:44:10 +0500

    added new file

---
+++ lib/services/newfile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class GitHub extends StatefulWidget {
+  const GitHub({super.key});
+
+  @override
+  State<GitHub> createState() => _GitHubState();
+}
+
+class _GitHubState extends State<GitHub> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
